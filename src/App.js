ddbedd7d2801e14e668f5c6995ed08940d2d84c1
Maddy <mmcintosh2k14@gmail.com>
2019-11-06 16:11:04 -0500

    adds redux imports to components and sets up mapStateToProps in the AdditionalFeatures.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+// redux imports
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// component imports
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+
+
+
+const store = createStore(reducer);
 
 const App = () => {
   const state = {
@@ -32,7 +39,8 @@ const App = () => {
   };
 
   return (
-    <div className="boxes">
+    <Provider store={store}>
+      <div className="boxes">
       <div className="box">
         <Header car={state.car} />
         <AddedFeatures car={state.car} />
@@ -41,7 +49,8 @@ const App = () => {
         <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
-    </div>
+      </div>
+    </Provider>
   );
 };
 
